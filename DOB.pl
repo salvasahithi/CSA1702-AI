f5d5b698d64633a28ce0bdf2e8c7e4e88bd5a6d9
salvasahithi <112721196+salvasahithi@users.noreply.github.com>
2023-10-18 12:20:01 +0530

    Add files via upload

---
+++ DOB.pl
@@ -0,0 +1,9 @@
+% Database of individuals with name and date of birth (DOB)
+% Syntax: person(Name, DOB).
+
+person(john, '1990-05-15').
+person(mary, '1985-11-20').
+person(alex, '1998-03-10').
+person(susan, '1976-09-05').
+person(steve, '2000-12-25').
+
